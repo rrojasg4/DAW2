af7a80c9adc6a45cb00ba0b5ff1dc183043b12d6
rrojasg4 <rrojasg4@gmail.com>
2021-02-04 00:56:14 +0100

    merge commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
-fetch("https://cat-fact.herokuapp.com/facts")
+/*fetch("https://cat-fact.herokuapp.com/facts")
 .then(response => response.json())
+.then(response => console.log(response))*/
+
+  fetch("https://aws.random.cat/meow")
+.then(response => response.json())
+.then(el => console.log(el))
+ 
+ 
+
+let container = document.getElementById("container")
+let image = document.createElement("IMG")
+fetch("https://aws.random.cat/meow", )
+.then(r =>r.json())
+.then(r =>{
+    image.setAttribute("src", r.file)
+})
+
+container.append(image)
+//https://aws.random.cat/meow */
+
+fetch("https://cat-fact.herokuapp.com/facts")
 .then(response => console.log(response))
 
-fetch("https://cloud-herokuapp.com/https://purr.objects-us-east-1.dream.io/i/Eu8F6.jpg")
-.then(response => response.json())
-.then(response => console.log(response))
+
+//fetch("https://cors-anywhere.herokuapp.com/https://aws.random.cat/meow").then(r=>r.json()).then(r=>console.log(r))
