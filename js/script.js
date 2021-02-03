97c4ade7a83d14af8d5b38baa603152686d7ecd7
ruben <rrojasg4@gmail.com>
2021-02-03 19:33:55 +0100

    1 API change

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-fetch("https://cat-fact.herokuapp.com/facts")
+fetch("https://catfact.ninja/facts")
 .then(response => response.json())
-.then(response => console.log(response))
+.then(response => console.log(response.data[0].fact))
 
 let src
 
@@ -10,6 +10,7 @@ fetch("https://aws.random.cat/meow")
     src = response.file
     let imagen = document.createElement("IMG")
     imagen.setAttribute("src", src)
+    imagen.classList.add("image")
     let container = document.getElementById("container")
     container.append(imagen)
 })
