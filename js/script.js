248e9bf085a56186068a8fbf12bc3f52e579ac8a
ruben <rrojasg4@gmail.com>
2021-02-03 21:23:14 +0100

    docs api

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 fetch("https://catfact.ninja/facts")
 .then(response => response.json())
 .then(response => console.log(response.data[0].fact))
-
+//https://documenter.getpostman.com/view/1946054/S11HvKSz?version=latest#61c57071-557d-4fc4-a677-ac822e71f35f
 let src
 
 fetch("https://aws.random.cat/meow")
