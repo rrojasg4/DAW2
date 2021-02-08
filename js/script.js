f3a59bc9500830b4fea733636af1ae3e66298af4
rrojasg4 <rrojasg4@gmail.com>
2021-02-08 23:28:01 +0100

    finished

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let factSpace = document.getElementById("factSpace")
 
 let new_fact = document.getElementById("new_fact")
 
-const generateBox = (r, file)=>{
+const generateBox = (r, file)=>{  //generate the items to append to the box with the facts and the img
   let box = document.createElement("DIV")
   let text = document.createElement("p")
   
@@ -20,27 +20,28 @@ const generateBox = (r, file)=>{
   factSpace.append(box)
 }
 
-const generateFact = ()=>{
+
+//API Key 7e18d3fc-422a-45bb-9618-60726532227d
+const generateFact = ()=>{  //get both img and text and generate the box
   fetch("https://catfact.ninja/facts")
   .then(response => response.json())
   .then(res=> {
 
-    fetch("https://aws.random.cat/meow")
-    .then(response => response.json())
+    fetch("https://api.thecatapi.com/v1/images/search?api_key=7e18d3fc-422a-45bb-9618-60726532227d")
+    .then(r =>r.json())
     .then(r => {
-
-      generateBox(res.data[0].fact, r.file)
+      generateBox(res.data[0].fact, r[0].url)
     })
   })
 }
 
-const deleteFacts = ()=>{
+const deleteFacts = ()=>{ //delete all children of a box, to swipe the first one and append the new one
   console.log("working")
   factSpace.childNodes.forEach(child =>{
     child.remove()
   })
 }
-const newFact = ()=>{
+const newFact = ()=>{ //generate a new box and delete the old ones
   deleteFacts()
   generateFact()
   deleteFacts()
@@ -49,21 +50,3 @@ generateFact()
 
 new_fact.addEventListener("click", newFact)
 
-/* 
-fetch("https://catfact.ninja/facts")
-.then(response => response.json())
-.then(response => console.log(response.data[0].fact))
-//https://documenter.getpostman.com/view/1946054/S11HvKSz?version=latest#61c57071-557d-4fc4-a677-ac822e71f35f
-let src
-
-fetch("https://aws.random.cat/meow")
-.then(response => response.json())
-.then(response => {
-    src = response.file
-    let imagen = document.createElement("IMG")
-    imagen.setAttribute("src", src)
-    imagen.classList.add("image")
-    let container = document.getElementById("container")
-    container.append(imagen)
-})
- */
